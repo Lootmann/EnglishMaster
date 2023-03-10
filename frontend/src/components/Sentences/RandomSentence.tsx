dd491ea51016c3ce0e614a3e019f61ff5fdd0fde
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-10 19:21:30 +0900

    frontend: Flip(not flip, just toggle show/hide btw) Random Sentence :^)

--- frontend/src/components/Sentences/RandomSentence.tsx
+++ frontend/src/components/Sentences/RandomSentence.tsx
@@ -1,6 +1,9 @@
-import React from "react";
 import axios from "axios";
+import React from "react";
+import { Link } from "react-router-dom";
+
 import { API_URL } from "../../utils/settings";
+import "../../styles/fonts.css";
 
 export function RandomSentence() {
   const [sentence, setSentence] = React.useState<SentenceType>({
@@ -9,17 +12,58 @@ export function RandomSentence() {
     translation: "",
   });
 
+  const [isFlip, setFlip] = React.useState<boolean>(false);
+
+  const [refresh, setRefresh] = React.useState<boolean>(false);
+
   React.useEffect(() => {
-    axios.get(API_URL + "/sentences/random").then((resp) => {
-      setSentence(resp.data);
+    axios.get(API_URL + "/sentences?random=true").then((resp) => {
+      setSentence(resp.data[0]);
+      console.log(resp.data[0]);
     });
-  }, []);
+  }, [refresh]);
 
   return (
     <>
       {sentence.id !== 0 && (
-        <div className="h-full flex gap-2">
-          <div className="p-4 text-xl">Random Sentence</div>
+        <div className="h-full w-2/3 ml-auto mr-auto flex flex-col gap-2 p-4">
+          <header className="flex gap-4 mb-4 items-baseline text-2xl">
+            <h2 className="text-2xl">Random Sentence</h2>
+          </header>
+
+          {/* TODO: flip card */}
+          <div
+            onClick={() => setFlip(!isFlip)}
+            className="h-1/2 mb-4 flex flex-col gap-4 p-4 border-2 border-slate-400 rounded-md"
+            onMouseDown={(e) => e.preventDefault()}
+          >
+            {isFlip ? (
+              <p
+                className="text-2xl transition-all"
+                onMouseDown={(e) => e.stopPropagation()}
+              >
+                {sentence.text}
+              </p>
+            ) : (
+              <p className="text-2xl" onMouseDown={(e) => e.stopPropagation()}>
+                {sentence.translation}
+              </p>
+            )}
+          </div>
+
+          <div className="flex justify-center gap-8">
+            <p className="text-3xl bg-slate-600 hover:bg-indigo-400 hover:text-slate-900 transition-all duration-200 px-3 py-1 rounded-md">
+              Read Count?
+            </p>
+
+            <Link
+              to={{}}
+              className="text-3xl bg-slate-600 hover:bg-teal-500 hover:text-slate-900 transition-all duration-200 px-3 py-1 rounded-md"
+              onClick={() => setRefresh(!refresh)}
+            >
+              Next Random
+            </Link>
+          </div>
         </div>
       )}
     </>
