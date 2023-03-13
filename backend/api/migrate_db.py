ba51714a590b33eddc56bee35e5276e43b7b5d7f
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-13 15:08:40 +0900

    backend: api/migrate_db: add CounterModel

--- backend/api/migrate_db.py
+++ backend/api/migrate_db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 
+from api.models.counters import Counter
 from api.models.sentences import Sentence
 from api.settings import Settings
 
@@ -11,6 +12,9 @@ engine = create_engine(setting.migrate_db_url, echo=True)
 def reset_database():
     Sentence.metadata.drop_all(bind=engine)
     Sentence.metadata.create_all(bind=engine)
+
+    Counter.metadata.drop_all(bind=engine)
+    Counter.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
