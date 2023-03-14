a4eaa0c4500289686ae120683dc213c3ae9ac1db
Lootmann <lyotsu.programming.or.some@gmail.com>
2023-03-14 14:00:48 +0900

    frontend: sentences aggregates - show from 7 days to today's Read Counts

--- frontend/src/sentences/aggregates/bar.tsx
+++ frontend/src/sentences/aggregates/bar.tsx
@@ -1,38 +1,60 @@
 import React from "react";
-import { isMonth, isToday, isWeek } from "../../utils/date";
+import { dateToString, isMonth, withinWeek } from "../../utils/date";
 import { ResponsiveBar } from "@nivo/bar";
 
+function initDateMap(): Map<string, number> {
+  const map = new Map<string, number>();
+  const date = new Date();
+  date.setDate(date.getDate() - 7);
+  for (let i = 0; i < 7; i++) {
+    date.setDate(date.getDate() + 1);
+    map.set(dateToString(date), 0);
+  }
+  return map;
+}
+
 export function Bar({ sentences }: any) {
+  /**
+   * bar data = [
+   *  {title: "", counts: <number>},
+   *  {title: "", counts: <number>},
+   *  ...
+   * ]
+   * @param sentences
+   * @returns Array
+   */
   function createBarData(sentences: SentenceType[]): Array<any> {
-    let [count_today, count_week, count_month] = [0, 0, 0];
+    let count_month = 0;
+
+    // create key by date between 7 days ago and today
+    // map has { "MM-DD": <number>, "MM-DD": <number>, }
+    const map = initDateMap();
 
     // FIXME: O(N^2) seems shit :^)
     // FIXME: create a dedicated API?
     for (const sentence of sentences) {
       for (const counter of sentence.counters) {
+        if (withinWeek(counter.created_at)) {
+          const date = dateToString(counter.created_at);
+          const value = map.get(date);
+          if (value !== undefined) {
+            map.set(date, value + 1);
+          } else {
+            map.set(date, 1);
+          }
+        }
+
         if (isMonth(counter.created_at)) count_month += 1;
-        if (isWeek(counter.created_at)) count_week += 1;
-        if (isToday(counter.created_at)) count_today += 1;
       }
     }
 
-    return [
-      {
-        title: "Today",
-        counts: count_today,
-        countsColor: "hsl(48, 70%, 50%)",
-      },
-      {
-        title: "Week",
-        counts: count_week,
-        countsColor: "hsl(229, 70%, 50%)",
-      },
-      {
-        title: "Month",
-        counts: count_month,
-        countsColor: "hsl(297, 70%, 50%)",
-      },
-    ];
+    const res = [];
+    for (const [key, value] of map.entries()) {
+      res.push({ title: key, counts: value });
+    }
+    res.push({ title: "Month", counts: count_month });
+
+    return res;
   }
 
   //  TODO: Bar Color
@@ -63,11 +85,12 @@ export function Bar({ sentences }: any) {
       // FIXME: not working :^)
       fill={[{ match: { id: "counts" }, id: "dots" }]}
       colors={{ scheme: "nivo" }}
+      colorBy="id"
       margin={{ top: 100, right: 100, bottom: 100, left: 100 }}
       padding={0.3}
       innerPadding={5}
       groupMode="stacked"
-      layout="horizontal"
+      layout="vertical"
       minValue={0}
       maxValue="auto"
       valueScale={{ type: "linear" }}
@@ -78,15 +101,15 @@ export function Bar({ sentences }: any) {
       enableGridX={true}
       enableGridY={false}
       // axis
-      axisTop={{
-        tickValues: "every 1",
-        tickSize: 6,
-        tickPadding: 10,
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        tickSize: 3,
+        tickPadding: 15,
         legendOffset: 5,
       }}
-      axisRight={null}
-      axisBottom={null}
       axisLeft={{
+        tickValues: "every 1",
         tickSize: 6,
         tickPadding: 5,
         legendOffset: 10,
--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -4,6 +4,34 @@ function getCurrentDate(): [number, number, number] {
   return [today.getFullYear(), today.getMonth() + 1, today.getDate()];
 }
 
+// padding 0 at head when getMonth() + 1 is between 0 and 9
+export function dateToString(dateString: Date): string {
+  const date = new Date(dateString);
+  const [month, day] = [date.getMonth() + 1, date.getDate()];
+
+  return `${month.toString().padStart(2, "0")}-${day
+    .toString()
+    .padStart(2, "0")}`;
+}
+
+/**
+ * withinWeek
+ * (from, to) = (7 days ago, today)
+ *
+ * @param {Date}dateString
+ * @returns
+ */
+export function withinWeek(dateString: Date): boolean {
+  const from = new Date();
+  from.setDate(from.getDate() - 7);
+  const to = new Date();
+  const createdAt = new Date(dateString);
+
+  return (
+    from.getTime() <= createdAt.getTime() && createdAt.getTime() <= to.getTime()
+  );
+}
+
 export function isMonth(dateString: Date): boolean {
   const date = new Date(dateString);
   const [year, month] = getCurrentDate();
